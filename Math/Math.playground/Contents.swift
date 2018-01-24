94ababcb611a7c47047ed50b0e9457530bf7289d
Luan Tran <noblakit01@gmail.com>
2018-01-24 19:30:23 +0700

    Add Math playground

---
+++ Math/Math.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+let sin0 = sin(0.0)
+let cos0 = cos(0.0)
+let cosPiPer22 = cos(90.0)
+let cosPiPer2 = cos(Double.pi * 0.5)
+let cos60 = cos(Double.pi / 3)
+let sin90 = sin(Double.pi * 0.5)
+let atan1 = atan(0.0)
+
